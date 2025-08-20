5317f3f6df5a95d2d52ac66418c2ac5beb186b84
tom7878787878 <thomastomer244@gmail.com>
2025-08-20 14:58:49 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()] });
+import { defineConfig } from "vite";
+import remix from "@remix-run/dev/vite";
+
+export default defineConfig({
+  plugins: [remix()],
+});
